3ed8ac519dee5df6852c2afe12d8ed04ba6d312d
theStrixCoder <mailme.bidyadhar123@gmail.com>
2025-03-04 01:35:17 +0530

    SystemUI: Re-evaluate system theme on UI mode change
    - Need for power menu to set accurate colors
    
    Squashed:
    From: Dhina17 <dhinalogu@gmail.com>
    Date: Tue, 4 Jun 2024 20:10:43 +0530
    Subject: [PATCH] fixup! SystemUI: Re-evaluate system theme on UI mode change
    
    Change-Id: I05d41eaf8ea19ce3b6ce659d01da33cf55de3b7e
    Signed-off-by: Saalim Quadri <danascape@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
+++ packages/SystemUI/src/com/android/systemui/theme/ThemeOverlayController.java
@@ -81,6 +81,8 @@ import com.android.systemui.monet.DynamicColors;
 import com.android.systemui.monet.Style;
 import com.android.systemui.monet.TonalPalette;
 import com.android.systemui.settings.UserTracker;
+import com.android.systemui.statusbar.policy.ConfigurationController;
+import com.android.systemui.statusbar.policy.ConfigurationController.ConfigurationListener;
 import com.android.systemui.statusbar.policy.DeviceProvisionedController;
 import com.android.systemui.statusbar.policy.DeviceProvisionedController.DeviceProvisionedListener;
 import com.android.systemui.tuner.TunerService;
@@ -149,6 +151,7 @@ public class ThemeOverlayController implements CoreStartable, Dumpable, TunerSer
     private final boolean mIsMonetEnabled;
     private final boolean mIsFidelityEnabled;
     private final UserTracker mUserTracker;
+    private final ConfigurationController mConfigurationController;
     private final DeviceProvisionedController mDeviceProvisionedController;
     private final Resources mResources;
     // Current wallpaper colors associated to a user.
@@ -204,6 +207,15 @@ public class ThemeOverlayController implements CoreStartable, Dumpable, TunerSer
     private boolean mDeferredThemeEvaluation;
     // Determines if we should ignore THEME_CUSTOMIZATION_OVERLAY_PACKAGES setting changes.
     private boolean mSkipSettingChange;
+
+    private final ConfigurationListener mConfigurationListener =
+            new ConfigurationListener() {
+                @Override
+                public void onUiModeChanged() {
+                    Log.i(TAG, "Re-applying theme on UI change");
+                    reevaluateSystemTheme(true /* forceReload */);
+                }
+            };
 
     private final DeviceProvisionedListener mDeviceProvisionedListener =
             new DeviceProvisionedListener() {
@@ -448,10 +460,12 @@ public class ThemeOverlayController implements CoreStartable, Dumpable, TunerSer
             KeyguardTransitionInteractor keyguardTransitionInteractor,
             UiModeManager uiModeManager,
             ActivityManager activityManager,
-            TunerService tunerService) {
+            TunerService tunerService,
+            ConfigurationController configurationController) {
         mContext = context;
         mIsMonetEnabled = featureFlags.isEnabled(Flags.MONET);
         mIsFidelityEnabled = featureFlags.isEnabled(Flags.COLOR_FIDELITY);
+        mConfigurationController = configurationController;
         mDeviceProvisionedController = deviceProvisionedController;
         mBroadcastDispatcher = broadcastDispatcher;
         mUserManager = userManager;
@@ -568,6 +582,7 @@ public class ThemeOverlayController implements CoreStartable, Dumpable, TunerSer
         }
 
         mUserTracker.addCallback(mUserTrackerCallback, mMainExecutor);
+        mConfigurationController.addCallback(mConfigurationListener);
         mDeviceProvisionedController.addCallback(mDeviceProvisionedListener);
 
         mTunerService.addTunable(this, PREF_CHROMA_FACTOR);
