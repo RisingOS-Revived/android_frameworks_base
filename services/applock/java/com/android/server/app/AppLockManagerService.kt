Kalpu-24/frameworks_base

service: Adapt for QuickSwitch
Change-Id: I6c8ea884d7bda45fa9649ac139027a01ce7e8da3

services/applock/java/com/android/server/app/AppLockManagerService.kt
@@ -819,6 +819,9 @@ class AppLockManagerService(
     }
 
     private fun enforceCallingPermission(msg: String) {
+        if (com.android.internal.util.lunaris.PixelPropsUtils.isSystemLauncher(Binder.getCallingUid())) {
+            return;
+        }
         context.enforceCallingPermission(Manifest.permission.MANAGE_APP_LOCK, msg)
     }
 

RisingOS-Revived/android_frameworks_base

services/applock/java/com/android/server/app/AppLockManagerService.kt
@@ -819,6 +819,9 @@ class AppLockManagerService(
     }
 
     private fun enforceCallingPermission(msg: String) {
+        if (com.android.internal.util.android.PixelPropsUtils.isSystemLauncher(Binder.getCallingUid())) {
+            return;
+        }
         context.enforceCallingPermission(Manifest.permission.MANAGE_APP_LOCK, msg)
     }
 